076c120a4cccd2a4e897a828dac8588b52272405
uturkarslan <ugurturkarslan@gmail.com>
2023-02-03 15:15:08 +0100

    Add infra dashboard

---
+++ newrelic/infra/terraform/01_variables.tf
@@ -0,0 +1,31 @@
+#################
+### Variables ###
+#################
+
+### General ###
+
+# New Relic account ID
+variable "NEW_RELIC_ACCOUNT_ID" {
+  type = string
+}
+
+# New Relic API key
+variable "NEW_RELIC_API_KEY" {
+  type = string
+}
+
+# New Relic region
+variable "NEW_RELIC_REGION" {
+  type = string
+}
+######
+
+# Cluster name
+variable "cluster_name" {
+  type = string
+}
+
+# Application names
+variable "app_names" {
+  type = list(string)
+}
